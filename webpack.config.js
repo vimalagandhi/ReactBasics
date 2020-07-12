b58f7969fe2d025203d17e6bc692ea28c7454f04
vimala <vimalabu96@gmail.com>
2020-07-12 19:19:54 +0530

    made changes in packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ var webpack = require("webpack");
 var path = require("path");
 
 var DIST_DIR = path.resolve(__dirname, "dist");
-var SRC__DIR = path.resolve(__dirname, "src");
+var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-    entry: SRC__DIR + "/app/index.js",
+    entry: SRC_DIR + "/app/index.js",
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
         publicPath: "/app/"
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js?/,
-                include: "SRC__DIR",
+                include: SRC_DIR,
                 loader: 'babel-loader',
                 query: {
                     presets: ["react", "es2015", "stage-2"]
