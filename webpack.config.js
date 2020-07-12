b6c8fd85fb19e00be37ee82b516f3c77dcbfcebd
vimala <vimalabu96@gmail.com>
2020-07-12 19:29:20 +0530

    made changes in packages

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ var config = {
                 include: SRC_DIR,
                 loader: 'babel-loader',
                 query: {
-                    presets: ["react", "es2015", "stage-2"]
+                    presets: ["react", "@babel/preset-env", "stage-2"]
                 }
             }
         ]
